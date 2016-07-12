383feab855e1b81bbb1e0c5317d6d7ddb449a871
ARMargolis <armargolis@gmail.com>
2016-07-12 08:10:59 -0400

    New version with much faster lineNextTableSQL
    Changes:
    lineNextTableSQL runs much faster
    WMATAtableSQL now returns two lineNexts: one with all the trains toward the North and East, another with all the toward the South and West
    Note: this code requires Pandas 0.18.1

--- MetroProjectSQL1Table.py
+++ MetroProjectSQL1Table.py
@@ -31,54 +31,95 @@ def saveWMATASQL(trainData, engine): #saves the current WMATA data to open engin
          for colName in ['Car','LocationCode','Line','DestinationCode','Min','Group']: #select the six relevant fields
              trainFrame.loc[iter][colName[:3]]=trainData[iter][colName] #and fill in the relevant data  
 	trainFrame.to_sql('WMATAFull', engine, if_exists='append') #send trainFrame to the SQL server
-	return
+	return trainFrame
 
 def WMATAtableSQL(timeMin,intervalSec): #records for timeMin minutes, about ever intervalSec seconds
 	import time, pandas as pd
 	from sqlalchemy import create_engine
 	engine = create_engine('postgresql+psycopg2://Original:tolistbtGU!@teamoriginal.ccc95gjlnnnc.us-east-1.rds.amazonaws.com:5432/WmataData') #opens the engine to WmataData
-	tableList=[] #creates a list of the table we're creating to add to the index 
+	 #creates a list of the table we're creating to add to the index 
+	isStart=True
 	startTime=time.time()
 	while time.time()<(startTime+60*timeMin): #runs for timeMin minutes
          stepStart=time.time()
-         tableList.append(saveWMATASQL(JSONfromMetro(getMetroStatus()),engine)) #save the current train data and appends the name to tableList
+         WMATAdf=saveWMATASQL(JSONfromMetro(getMetroStatus()),engine) #save the current train data and appends the name to tableList
+         if len(WMATAdf.index)>0: #if you got data back
+             if isStart: #and it's the first row
+                 allLNtoNE=allLNtoNE4(WMATAdf) #set allLNtoNE equal to the all LineNext to NE data
+                 allLNtoSW=allLNtoSW4(WMATAdf) #set allLNtoSW equal to the all LineNext to SW data
+                 isStart=False #and the next row will not be the first row
+             else: #for other rows
+                 allLNtoNE=allLNtoNE.append(allLNtoNE4(WMATAdf)) #append the data
+                 allLNtoSW=allLNtoSW.append(allLNtoSW4(WMATAdf))
          stepTime=time.time()-stepStart #calculates the time this step took
          if stepTime<intervalSec: #if intervalSec seconds have not passed,
              time.sleep(intervalSec-stepTime) #wait until a total of intervalSec have passed
 	engine.connect().close()
+	return [allLNtoNE, allLNtoSW]
+
+def lineNextDF(line, destList, arrData):
+	import pandas as pd
+	timeString=arrData.DT.iloc[0]
+	rowName=pd.to_datetime('2016-'+timeString[0]+'-'+timeString[1:3]+' '+timeString[3:5]+':'+timeString[5:7]+':'+timeString[7:])
+# names the row as a timestamp with the month        day                  hour                  minute             second
+	lineStat=pd.DataFrame('-',index=[rowName],columns=line)
+	for station in line: #repeat the below process for every station on the line
+		trains2consider=arrData.loc[lambda df: df.Loc==station].loc[lambda df: df.Des.isin(destList)] #pull out the trains at that station heading toward the destinations
+		if len(trains2consider.index)>0: #If you found a train
+			lineStat.loc[rowName][station]=trains2consider.Lin.iloc[0]+':'+trains2consider.Min.iloc[0] #set the station status to the color and ETA of the first arriving train
+	return lineStat
+
+def allLNtoNE4(arrData): #all of the lines to the North and East during Surge 4
+    import pandas as pd
+    #these are the lines to examine. lowercase letters (w,e,n,s,c) are the ordinal directions and central. Uppercase letters are the lines
+    lineList=[wOEnd, wSEnd, SOLine, ['C07'], BArlCem, SOBLine, eOLine, SBLine, sGLine, cGYLine, nGYEnd, wRedEnd, cRedLine, eRedEnd]
+    #these are the destinations along those lines. D13=New Car... G05=Largo E01=Mt Vernon E10=Greenbelt B08=Silver Spring B11=Glenmont    
+    destList=[['D13'],['G05'],['D13','G05'],['G05','E01','E10'],['G05'],['G05','D13','G05'],['D13'],['G05'],['E10'],['E01','E10'],['E10'],['B11'],['B08','B11'],['B11']]
+    LNlist=[]
+    for num in range(len(lineList)):
+        LNlist.append(lineNextDF(lineList[num], destList[num], arrData)) #run for each line and destination
+    return pd.concat(LNlist, axis=1, join='outer') #then join them all together
+
+def allLNtoSW4(arrData): #all of the lines to the South and West during Surge 4
+    import pandas as pd
+    #these are the lines to examine. lowercase letters (w,e,n,s,c) are the ordinal directions and central. Uppercase letters are the lines
+    lineList=[wOEnd, wSEnd, SOLine, ['C07'], BArlCem, SOBLine, eOLine, SBLine, sGLine, cGYLine, nGYEnd, wRedEnd, cRedLine, eRedEnd]
+    lineList.reverse() #reverse the list
+    #these are the destinations along those lines. K08=Vienna N06=Wiehle C08=Pentagon City F11=Branch Ave A15=Shady Gr A11=Grovesnor
+    destList=[['K08'],['N06'],['K08','N06'],['C08'],['C08'],['K08','C08','N06'],['K08'],['C08','N06'],['F11'],['F11', 'C08'],['F11', 'C08'],['A15'],['A11','A15'],['A15']]
+    lineList.reverse() #reverse the list
+    LNlist=[]
+    for num in range(len(lineList)):
+        lineList[num].reverse() #reverse the order of the stations in the list
+        LNlist.append(lineNextDF(lineList[num], destList[num], arrData)) #run for each line and destination
+    return pd.concat(LNlist, axis=1, join='outer') #then join them all together
 
 def lineNextSQL(line, timeString,destList, engine): #reads the next train to arrive at the stations in line heading toward destList and returns it as a Data Frame
 	import pandas as pd
 	from sqlalchemy import create_engine
-	if engine is None:  
+	isEngineNone=(engine is None)
+	if isEngineNone: #if there's not an engine, make one  
 		engine = create_engine('postgresql+psycopg2://Original:tolistbtGU!@teamoriginal.ccc95gjlnnnc.us-east-1.rds.amazonaws.com:5432/WmataData')
-	rowName=pd.to_datetime('2016-'+timeString[0]+'-'+timeString[1:3]+' '+timeString[3:5]+':'+timeString[5:7]+':'+timeString[7:])
-# names the row as a timestamp with the month        day                  hour                  minute             second
-	lineStat=pd.DataFrame('-',index=[rowName],columns=line)
 	query='SELECT * FROM "WMATAFull" WHERE "DT"='+"'"+timeString+"';"
 	arrData=pd.read_sql(query,engine)
-	for station in line: #repeat the below process for every station on the line
-		trainNum=0 #the below lines look for the first train going to a destination in destList
-		while trainNum<len(arrData.index) and (arrData.loc[trainNum]['Loc']!=station or arrData.loc[trainNum]['Des'] not in destList):
-			trainNum+=1
-		if trainNum<len(arrData.index): #If you found a train
-			lineStat.loc[rowName][station]=arrData.loc[trainNum]['Lin']+':'+arrData.loc[trainNum]['Min'] #set the station status to the color and ETA of arriving train
-	if engine is None: 
+	if isEngineNone: 
 		engine.connect().close()
-	return lineStat
+	return lineNextDF(line, destList, arrData)
 
-def lineNextTableSQL(line, firstTime,destList, iterNum): #saves the next train arrivals for a line and destList over time
-	import pandas as pd
+def lineNextTableSQL(line, firstTime, lastTime, destList): #saves the next train arrivals for a line and destList over time
+	import time, pandas as pd
 	from sqlalchemy import create_engine
+	print(time.strftime("%a, %d %b %Y %H:%M:%S"))
 	engine = create_engine('postgresql+psycopg2://Original:tolistbtGU!@teamoriginal.ccc95gjlnnnc.us-east-1.rds.amazonaws.com:5432/WmataData')
-	lineStats=lineNextSQL(line,firstTime, destList, engine) #save the first stattus
-	query='SELECT DISTINCT "DT" FROM "WMATAFull" ORDER BY "DT" ASC;'
-	timesPD=pd.read_sql(query,engine)
-	firstRow=list(timesPD.T.loc['DT']).index(firstTime)
-	for num in range(1,min(iterNum,len(timesPD.index)-firstRow)): #run for iterNum or until the end
-  		numTime=timesPD.iloc[firstRow+num][0]
-  		lineStats=lineStats.append(lineNextSQL(line,numTime, destList, engine))
+	query='SELECT * FROM "WMATAFull" WHERE "DT">='+"'"+firstTime+"' AND "+'"DT"<='+"'"+lastTime+"';"
+	arrData=pd.read_sql(query,engine)
+	print(time.strftime("%a, %d %b %Y %H:%M:%S"))
+	timesPD=arrData.DT.value_counts().index #pull out each time and call it timesPD
+	lineStats=lineNextDF(line, destList, arrData.loc[lambda df: df.DT==timesPD[0]]) #save the first status
+	for num in range(1,len(timesPD)): #for each time
+  		lineStats=lineStats.append(lineNextDF(line, destList, arrData.loc[lambda df: df.DT==timesPD[num]])) #add the data for that time
 	engine.connect().close() 
+	print(time.strftime("%a, %d %b %Y %H:%M:%S"))
 	return lineStats
 
 def trainBuild(lineStat,startTime): #determines how long it took the train arriving after startTime to reach every station and returns it as one row data frame
@@ -96,9 +137,9 @@ def trainBuild(lineStat,startTime): #determines how long it took the train arriv
 	for stationNum in range(1,len(lineStat.columns)): #this fills in the difference arrival time for every station
          isTrainBoarding=False
          while timeRow<(len(lineStat.index)-1) and not isTrainBoarding: #while timeRow is in bounds and the train is not boarding
-            timeRow+=1 #go to the next line
             #The line below says that a train is boarding if either it has status "BRD" or it has status "ARR" and 20 seconds later the station is waiting for a different train 
             isTrainBoarding=lineStat.iloc[timeRow][stationNum]==(specTrain.loc[startTime]['Col']+":BRD") or (lineStat.iloc[timeRow][stationNum]==(specTrain.loc[startTime]['Col']+":ARR") and (lineStat.iloc[timeRow+1][stationNum][:2]!=specTrain.loc[startTime]['Col']))
+            timeRow+=1 #go to the next line
          if timeRow>=len(lineStat.index)-1: #if you get to the end,
              return [specTrain, skipRows] #just return what you have
          timeDif=lineStat.index[timeRow]-startTime #set timeDif to the diffence between arrival at this station and startTime
@@ -119,7 +160,7 @@ def trainTable(lineStat): #returns a table listing the trains by start time, col
         while rowNum<len(lineStat.index)-1 and lineStat.iloc[rowNum][0]==currentColor+':BRD': #while the train (with currentColor) is boarding,
             rowNum+=1 #go to the next row
         [newTrain, skipRows]=trainBuild(lineStat,lineStat.index[rowNum]) #once you've gotten to a new train arrival, record it as newTrain and note the rows to skip 
-        if (newTrain.index[0]-masterTable.index[-1]).seconds>int(masterTable.iloc[-1][-1])-int(newTrain.iloc[0][-1]): #if newTrain arrived at the last station after the last train
+        if newTrain.iloc[0][-1]=='0' or (newTrain.index[0]-masterTable.index[-1]).seconds>int(masterTable.iloc[-1][-1])-int(newTrain.iloc[0][-1]): #if you've reached the end or newTrain arrived at the last station after the last train
             masterTable=masterTable.append(newTrain) #append newTrain to the masterTable
         else: #but if that's not the case, something went wrong with the last train
             masterTable=pd.concat([masterTable.iloc[:][:-1],newTrain]) #replace the last row of masterTable with the data for newTrain
@@ -127,13 +168,23 @@ def trainTable(lineStat): #returns a table listing the trains by start time, col
         rowNum+=skipRows+1 #skip ahead to the next train
     return masterTable
 
-#Stations from Rosslyn to Stadium-Armory (Silver Orange Blue) to serve as input for LineStat
+#these are the lines to examine. lowercase letters (w,e,n,s,c) are the ordinal directions and central. Uppercase letters (O,S,B,Y,G) and Red are the lines
+wOEnd=['K07','K06','K05']
+wSEnd=['N04','N03','N02','N01']
+SOLine=['K04','K03','K02','K01']
+wBEnd=['J02']
+sYEnd=['C14','C15']
+BYLine=['C07','C08','C09','C10','C12','C13']
+BArlCem=['C06']
 SOBLine=['C05', 'C04', 'C03', 'C02', 'C01', 'D01', 'D02', 'D03', 'D04', 'D05', 'D06', 'D07', 'D08']
-CenRedLine=['A11', 'A10', 'A09', 'A08', 'A07', 'A06', 'A05', 'A04', 'A03', 'A02', 'A01', 'B01', 'B02', 'B03', 'B35', 'B04', 'B05', 'B06', 'B07', 'B08']
-RedLine=['A15','A14','A13', 'A12']+CenRedLine+['B09','B10','B11']
-GYLine=['E09', 'E08', 'E07', 'E06', 'E05', 'E04', 'E03', 'E02', 'E01', 'F01', 'F02', 'F03']
-GrLine=['E09', 'E08', 'E07', 'E06', 'E05', 'E04', 'E03', 'E02', 'E01', 'F01', 'F02', 'F03','F04','F05','F06','F07','F08','F09','F10']
-SBLine=['J02','C13','C12','C10','C09','C08','C07','C06']
+SBLine=['G01','G02','G03','G04']
+eOLine=['D09','D10','D11','D12']
+sGLine=['F10','F09', 'F08', 'F07', 'F06', 'F05', 'F04']
+cGYLine=['F03', 'F02', 'F01']
+nGYEnd=['E01', 'E02', 'E03', 'E04', 'E05', 'E06', 'E07', 'E08', 'E09']
+wRedEnd=['A14','A13', 'A12', 'A11']
+cRedLine=['A10', 'A09', 'A08', 'A07', 'A06', 'A05', 'A04', 'A03', 'A02', 'A01', 'B01', 'B02', 'B03', 'B35', 'B04', 'B05', 'B06', 'B07']
+eRedEnd=['B08','B09','B10']
 
 #minDist is used by trainBuild to determine the minimum time between stations, in case there are two trains on the same line boarding at adjacent stations
 minDist={'A01': {'A02': {'weight': 4}, 'B01': {'weight': 2}},
